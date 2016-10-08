9172fa622b879df0a5cda76e4cab470be4ad9c7a
conormm <conrmcdonald@gmail.com>
2016-10-08 16:08:24 +0100

    This is me testing out pycharm and github

---
+++ test_script.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import cross_val_score
+
+data = load_iris()
+X = data.data()
+y = data.target()
+
+clf_ada = AdaBoostClassifier()
+clf_ada.fit(X, y)
+clf_ada.feature_importances_
+
+cv_scores = cross_val_score(clf_ada, X, y, cv=5)
