e2d459210ffa02cfb72056178661c522ba15d4bf
conormm <conrmcdonald@gmail.com>
2016-10-09 20:00:39 +0100

    updated

--- debate_2.R
+++ debate_2.R
@@ -10,6 +10,36 @@ library(stringr)
 library(broom)
 library(ggrepel)
 
+# load data / fix column names
+
+debate_2 <- read_csv("/Users/conormcdonald/Desktop/second_debate.csv")
+names(debate_2) <- tolower(names(df))
+
+# code for getting negative sentiments from debate 2
+
+debate_words_2 <- debate_2 %>% 
+  unnest_tokens(word, text) %>% 
+  filter(!word %in% stop_words$word,
+         str_detect(word, "^[a-z']+$"))
+
+sent_2 <- sentiments %>%
+  filter(lexicon == "AFINN") %>%
+  select(word, afinn_score = score)
+
+sent_line_2 <- debate_words_2 %>% 
+  inner_join(sent, by = "word") %>% 
+  group_by(speaker, line) %>% 
+  summarise(sentiment = mean(afinn_score))
+
+neg_lines_2 <- sent_line_2 %>% 
+  group_by(speaker) %>% 
+  mutate(lines = n()) %>% 
+  filter(sentiment < 0) %>% 
+  mutate(neg = n(), 
+         prop_neg = neg / lines) %>% 
+  ungroup() %>% 
+  distinct(speaker, lines, neg, prop_neg)
+
 # establish priors based on use of negative sentiment in previous debate
 
 trump_b0 <- eb$beta1[eb$speaker == "Trump"]
